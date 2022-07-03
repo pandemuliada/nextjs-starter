ead99f8d72a769dbd57c7db86614865014e28ae9
pandemuliada <pandemuliada@gmail.com>
2022-07-03 09:40:13 +0800

    :sparkles: setup recoil & next image

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
     locales: ["en", "id"],
     defaultLocale: "en",
   },
+  // Add your image storage here when using next/image
+  images: {
+    domains: ["your.external-image-storage.com"],
+  },
 };
 
 module.exports = nextConfig;
