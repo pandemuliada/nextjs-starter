1f80257e124716c44d314cf3fc4c1c7485a7c936
pandemuliada <pandemuliada@gmail.com>
2022-08-11 14:31:46 +0800

    :sparkles: fix drawer

--- components/ui/Drawer.js
+++ components/ui/Drawer.js
@@ -1,12 +1,6 @@
 import { AnimatePresence, m, motion } from "framer-motion";
 import Portal from "../Portal";
 
-const drawerAnimations = {
-  hidden: { opacity: 0, right: "-100%" },
-  show: { opacity: 1, right: 0 },
-  exit: { opacity: 0, right: "-100%" },
-};
-
 const Drawer = (props) => {
   const { show, onClose, children, className } = props;
 
@@ -22,7 +16,7 @@ const Drawer = (props) => {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
-              transition={{ type: "tween", duration: 0.7, ease: "easeIn" }}
+              transition={{ type: "tween", duration: 0.4, ease: "easeIn" }}
             />
             {/* backdrop */}
 
@@ -30,11 +24,10 @@ const Drawer = (props) => {
             <motion.div
               className={`fixed z-[1] top-0 right-0 bottom-0 bg-white ${className}`}
               style={{ width: 400 }}
-              variants={drawerAnimations}
-              initial="hidden"
-              animate="show"
-              exit="exit"
-              transition={{ type: "tween", duration: 0.7, ease: "easeInOut" }}
+              initial={{ opacity: 0, right: -400 }}
+              animate={{ opacity: 1, right: 0 }}
+              exit={{ opacity: 0, right: -400 }}
+              transition={{ type: "tween", duration: 0.4, ease: "easeInOut" }}
             >
               {children}
             </motion.div>
