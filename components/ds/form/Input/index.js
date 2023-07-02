3621bc5a96f725f5064a43dfd4a8958f854c45da
pandemuliada <pandemuliada@gmail.com>
2023-07-02 11:36:34 +0800

    :recycle: moving input components

---
+++ components/ds/form/Input/index.js
@@ -0,0 +1,11 @@
+import TextInput from "./TextInput";
+import PasswordInput from "./PasswordInput";
+import Textarea from "./Textarea";
+
+const Input = {
+  Text: TextInput,
+  Password: PasswordInput,
+  Textarea: Textarea,
+};
+
+export default Input;
--- components/ds/form/index.js
+++ components/ds/form/index.js
@@ -1,22 +1,6 @@
-import TextInput from "./TextInput";
-import PasswordInput from "./PasswordInput";
-import Checkbox from "./Checkbox";
-import DefaultSelect from "./DefaultSelect";
-import Textarea from "./Textarea";
-import CustomSelect from "./CustomSelect";
+import Checkbox from "../Checkbox";
+import DefaultSelect from "../DefaultSelect";
+import CustomSelect from "../CustomSelect";
+import Input from "@/components/ds/form/Input";
 
-const Input = {
-  Text: TextInput,
-  Password: PasswordInput,
-  Textarea: Textarea,
-};
-
-export {
-  Input,
-  // TextInput,
-  // PasswordInput,
-  // Textarea,
-  Checkbox,
-  DefaultSelect,
-  CustomSelect,
-};
+export { Input, Checkbox, DefaultSelect, CustomSelect };
